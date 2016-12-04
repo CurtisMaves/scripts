bbb0504539c8190b873b79dc1ce7e75406aa982e
Curtis Maves <curtismaves@gmail.com>
2016-12-03 20:31:42 -0500

    Various changes to copyFromGit.sh

--- copyFromGit.sh
+++ copyFromGit.sh
@@ -1,21 +1,20 @@
 #!/bin/sh
 
-mkdir -p /tmp/$USER/gitcopy
-chmod 700 /tmp/$USER/gitcopy
+mkdir -p /tmp/$SUDO_USER/gitcopy
+chmod 700 /tmp/$SUDO_USER/gitcopy
 
-cp -rf ~/scripts/* /tmp/$USER/gitcopy 
+cp -rf /home/$SUDO_USER/scripts/* /tmp/$SUDO_USER/gitcopy 
 
 
 #replace insert********** with your relavant info
-chmod 700 /tmp/$USER/gitcopy/vpn-up.sh
-sed 's/\$dUser=<-username->/\$dPass=insertUserHere/' /tmp/$USER/gitcopy/vpn-up.sh
-sed 's/\$dPass=<-password->/\$dPass=insertPasswordHere/'/tmp/$USER/gitcopy/vpn-up.sh
+chmod 700 /tmp/$SUDO_USER/gitcopy/vpn-up.sh
+sed -i's/\$dUser=<-username->/\$dUass=insertUserHere/' /tmp/$SUDO_USER/gitcopy/vpn-up.sh
+sed -i 's/\$dPass=<-password->/\$dPass=insertPasswordHere/'/tmp/$SUDO_USER/gitcopy/vpn-up.sh
 
-cp -f /tmp/$USER/gitcopy/vpn-up.sh /etc/NetworkManager/dispatcher.d/
-cp -f /tmp/$USER/gitcopy/nopvn.sh /etc/NetworkManager/dispatcher.d/
-cp -f /tmp/$USER/gitcopy/up_down.sh /etc/NetworkManager/dispatcher.d/
+cp -f /tmp/$SUDO_USER/gitcopy/vpn-up.sh /etc/NetworkManager/dispatcher.d/
+cp -f /tmp/$SUDO_USER/gitcopy/nopvn.sh /etc/NetworkManager/dispatcher.d/
 chmod u+x /etc/NetworkManager/dispatcher.d/*
 chmod a-x /etc/NetworkManager/dispatcher.d/output.txt
 
-rm -rvf /tmp/$USER/gitcopy
+rm -rf /tmp/$USER/gitcopy
 
