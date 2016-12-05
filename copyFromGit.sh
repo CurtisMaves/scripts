89f355bd61aea6994b1e63e47f4b1e149e8a05b1
Curtis Maves <cmaves@purdue.edu>
2016-12-05 13:52:40 -0500

    fixed vpn missplelling

--- copyFromGit.sh
+++ copyFromGit.sh
@@ -12,7 +12,7 @@ sed -i 's/dUser=<-username->/dUass=insertUserHere/' /tmp/$SUDO_USER/gitcopy/vpn-
 sed -i 's/dPass=<-password->/dPass=insertPasswordHere/' /tmp/$SUDO_USER/gitcopy/vpn-up.sh
 
 cp -f /tmp/$SUDO_USER/gitcopy/vpn-up.sh /etc/NetworkManager/dispatcher.d/
-cp -f /tmp/$SUDO_USER/gitcopy/nopvn.sh /etc/NetworkManager/dispatcher.d/
+cp -f /tmp/$SUDO_USER/gitcopy/novpn.sh /etc/NetworkManager/dispatcher.d/
 chmod u+x /etc/NetworkManager/dispatcher.d/*
 chmod a-x /etc/NetworkManager/dispatcher.d/output.txt
 
