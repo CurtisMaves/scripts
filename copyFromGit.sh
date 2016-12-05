29feb765607e92f06c7d6f9233f01679b8271317
Curtis Maves <cmaves@purdue.edu>
2016-12-05 13:36:19 -0500

    fixed copyfromgit.sh and vpn-up.sh

--- copyFromGit.sh
+++ copyFromGit.sh
@@ -8,8 +8,8 @@ cp -rf ~/scripts/* /tmp/$USER/gitcopy
 
 #replace insert********** with your relavant info
 chmod 700 /tmp/$USER/gitcopy/vpn-up.sh
-sed 's/\$dUser=<-username->/\$dPass=insertUserHere/' /tmp/$USER/gitcopy/vpn-up.sh
-sed 's/\$dPass=<-password->/\$dPass=insertPasswordHere/'/tmp/$USER/gitcopy/vpn-up.sh
+sed 's/\dUser=<-username->/\dPass=insertUserHere/' /tmp/$USER/gitcopy/vpn-up.sh
+sed 's/\dPass=<-password->/\dPass=insertPasswordHere/'/tmp/$USER/gitcopy/vpn-up.sh
 
 cp -f /tmp/$USER/gitcopy/vpn-up.sh /etc/NetworkManager/dispatcher.d/
 cp -f /tmp/$USER/gitcopy/nopvn.sh /etc/NetworkManager/dispatcher.d/
--- vpn-up.sh
+++ vpn-up.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-$dUser=<-username->
-$dPass=<-password->
+dUser=<-username->
+dPass=<-password->
 
 date >> /etc/NetworkManager/dispatcher.d/output.txt 2>&1
 echo $2 >> /etc/NetworkManager/dispatcher.d/output.txt 2>&1
@@ -16,7 +16,7 @@ systemctl status deluged || systemctl start deluged && sleep 2
 systemctl start deluged >> /etc/NetworkManager/dispatcher.d/output.txt 2>&1
 systemctl start syncthing@curtis.service >> /etc/NetworkManager/dispatcher.d/output.txt 2>&1
 
-IP_ADDR="$(ip addr show tun0 | grep -o -E 'inet [[:digit:]]{1,3}\.[[:digit:]]{1,3}\.[[:digit:]]{1,3}\.[[:digit:]]{1,3}' | grep -o -E '[^inet ]{5}.*')"
+IP_ADDR=$(ip addr show tun0 | grep -o -E 'inet [[:digit:]]{1,3}\.[[:digit:]]{1,3}\.[[:digit:]]{1,3}\.[[:digit:]]{1,3}' | grep -o -E '[^inet ]{5}.*')
 
 echo $IP_ADDR >> /etc/NetworkManager/dispatcher.d/output.txt 2>&1
 deluge-console "connect localhost $dUser  $uPass; config --set listen_interface $IP_ADDR" >> /etc/NetworkManager/dispatcher.d/output.txt 2>&1
