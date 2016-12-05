7bf132551eef6b1e6c88ecaa828e83c7596cec92
Curtis Maves <cmaves@purdue.edu>
2016-12-05 13:48:34 -0500

    fixed sed

--- copyFromGit.sh
+++ copyFromGit.sh
@@ -8,8 +8,8 @@ cp -rf /home/$SUDO_USER/scripts/* /tmp/$SUDO_USER/gitcopy
 
 #replace insert********** with your relavant info
 chmod 700 /tmp/$SUDO_USER/gitcopy/vpn-up.sh
-sed -i 's/\dUser=<-username->/\dUass=insertUserHere/' /tmp/$SUDO_USER/gitcopy/vpn-up.sh
-sed -i 's/\dPass=<-password->/\dPass=insertPasswordHere/'/tmp/$SUDO_USER/gitcopy/vpn-up.sh
+sed -i 's/dUser=<-username->/dUass=insertUserHere/' /tmp/$SUDO_USER/gitcopy/vpn-up.sh
+sed -i 's/dPass=<-password->/dPass=insertPasswordHere/'/tmp/$SUDO_USER/gitcopy/vpn-up.sh
 
 cp -f /tmp/$SUDO_USER/gitcopy/vpn-up.sh /etc/NetworkManager/dispatcher.d/
 cp -f /tmp/$SUDO_USER/gitcopy/nopvn.sh /etc/NetworkManager/dispatcher.d/
