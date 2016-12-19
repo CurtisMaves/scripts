fff657ab34787c4c9ae4f6b83665b0ba846265ea
Curtis Maves <cmaves@purdue.edu>
2016-12-19 11:47:58 -0500

    Made copyFromGit.sh verbose

--- copyFromGit.sh
+++ copyFromGit.sh
@@ -1,20 +1,20 @@
 #!/bin/sh
 
-mkdir -p /tmp/$SUDO_USER/gitcopy
-chmod 700 /tmp/$SUDO_USER/gitcopy
+mkdir -vp /tmp/$SUDO_USER/gitcopy
+chmod -v 700 /tmp/$SUDO_USER/gitcopy
 
-cp -rf /home/$SUDO_USER/scripts/* /tmp/$SUDO_USER/gitcopy 
+cp -rvf /home/$SUDO_USER/scripts/* /tmp/$SUDO_USER/gitcopy 
 
 
 #replace insert********** with your relavant info
-chmod 700 /tmp/$SUDO_USER/gitcopy/vpn-up.sh
+chmod -v 700 /tmp/$SUDO_USER/gitcopy/vpn-up.sh
 sed -i 's/dUser=<-username->/dUass=insertUserHere/' /tmp/$SUDO_USER/gitcopy/vpn-up.sh
 sed -i 's/dPass=<-password->/dPass=insertPasswordHere/' /tmp/$SUDO_USER/gitcopy/vpn-up.sh
 
-cp -f /tmp/$SUDO_USER/gitcopy/vpn-up.sh /etc/NetworkManager/dispatcher.d/
-cp -f /tmp/$SUDO_USER/gitcopy/novpn.sh /etc/NetworkManager/dispatcher.d/
-chmod u+x /etc/NetworkManager/dispatcher.d/*
-chmod a-x /etc/NetworkManager/dispatcher.d/output.txt
+cp -vf /tmp/$SUDO_USER/gitcopy/vpn-up.sh /etc/NetworkManager/dispatcher.d/
+cp -vf /tmp/$SUDO_USER/gitcopy/novpn.sh /etc/NetworkManager/dispatcher.d/
+chmod -v u+x /etc/NetworkManager/dispatcher.d/*
+chmod -v a-x /etc/NetworkManager/dispatcher.d/output.txt
 
-rm -rf /tmp/$SUDO_USER/gitcopy
+rm -rvf /tmp/$SUDO_USER/gitcopy
 
