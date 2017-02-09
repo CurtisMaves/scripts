fa7bb2262ea087cd8644aa7ce644a3296be4ccad
Curtis Maves <cmaves@purdue.edu>
2017-02-09 14:22:48 -0500

    Removed output to text file. Better to run it manually if you need to debug

--- vpn-up.sh
+++ vpn-up.sh
@@ -3,8 +3,8 @@
 dUser=<-username->
 dPass=<-password->
 
-date >> /etc/NetworkManager/dispatcher.d/output.txt 2>&1
-echo $2 >> /etc/NetworkManager/dispatcher.d/output.txt 2>&1
+date
+echo $2
 if [ "$2" != "vpn-up" ]; then
 	echo 'exit' >> /etc/NetworkManager/dispatcher.d/output.txt 2>&1
 	exit
@@ -15,14 +15,11 @@ sysctl -w net.ipv6.conf.all.disable_ipv6=1
 sysctl -w net.ipv4.conf.$1.rp_filter=0
 sysctl -w net.ipv4.conf.default.rp_filter=0
 sysctl -w net.ipv4.conf.all.rp_filter=0
-systemctl status deluged || systemctl start deluged && sleep 2
-systemctl start deluged >> /etc/NetworkManager/dispatcher.d/output.txt 2>&1
-systemctl start syncthing@curtis.service >> /etc/NetworkManager/dispatcher.d/output.txt 2>&1
+systemctl start deluged
+systemctl start syncthing@curtis.service
 
 IP_ADDR=$(ip addr show tun0 | grep -o -E 'inet [[:digit:]]{1,3}\.[[:digit:]]{1,3}\.[[:digit:]]{1,3}\.[[:digit:]]{1,3}' | grep -o -E '[^inet ]{5}.*')
 
-echo $IP_ADDR >> /etc/NetworkManager/dispatcher.d/output.txt 2>&1
-deluge-console "connect localhost $dUser  $dPass; config --set listen_interface $IP_ADDR" >> /etc/NetworkManager/dispatcher.d/output.txt 2>&1
-echo "connect localhost $dUser $dPass; config --set listen_address $IP_ADDR" >> /etc/NetworkManager/dispatcher.d/output.txt 2>&1
-
-systemctl restart deluged >> /etc/NetworkManager/dispatcher.d/output.txt 2>&1
+echo $IP_ADDR
+echo "connect localhost $dUser $dPass; config --set listen_address $IP_ADDR"
+deluge-console "connect localhost $dUser  $dPass; config --set listen_interface $IP_ADDR"
